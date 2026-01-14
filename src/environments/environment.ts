f4f187e460f608abe4700280c9e50c1852ea06be
janhavi-sharma <janhavi.p.sharma@gmail.com>
2026-01-14 22:53:27 +0530

    Remove environment files and ignore sensitive config

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import { producerNotifyConsumers } from "@angular/core/primitives/signals";
-
-export const environment = {
-  production: false,  
-  supabaseUrl: 'https://bwsvbpmjetkypstxomjs.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3c3ZicG1qZXRreXBzdHhvbWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyMjUwMTIsImV4cCI6MjA4MzgwMTAxMn0.yA9BipzH8tLmXADOmXb2CRndcLf6ZmT0zI8E0v_tV9E'
-};
